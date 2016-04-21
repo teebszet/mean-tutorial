a1c9eee171c855565c4c37ee68f5833ccd761963
Tim Man <timothyc.man@gmail.com>
2016-04-21 16:53:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.factory('posts', [function () {
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+app.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function ($scope, posts) {
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function () {
+      if (!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'tim', body: 'this is shit', upvotes: 0},
+          {author: 'guy', body: 'yeah, agree with above', upvotes: 0}
+        ]
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+
+    $scope.incrementUpvotes = function (post) {
+      post.upvotes += 1;
+    };
+  }
+]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function ($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function () {
+      if (!$scope.body || $scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: $scope.author || 'anon',
+        upvotes: 0,
+      });
+      $scope.body = '';
+      $scope.author = '';
+    };
+
+    $scope.incrementUpvotes = function (comment) {
+      comment.upvotes += 1;
+    };
+  }
+]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl',
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl',
+      });
+    $urlRouterProvider.otherwise('home');
+  }
+]);
